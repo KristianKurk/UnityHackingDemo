0cdf6545a685d89d637a88084178d7456a1fb6c5
Kristian Kurktchiev <43147153+KristianKurk@users.noreply.github.com>
2020-02-17 09:21:00 -0500

    The files you'll need

---
+++ Assets/SafeInt.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct SafeInt
+{
+    private int offset;     //a randomly generated value.
+    private int value;      //our secured value.
+
+    public SafeInt(int value = 0)
+    {
+        //Assign a random value to our offset.
+        offset = Random.Range(-1000, 1000);
+        //Set the secure value.
+        this.value = value + offset;
+    }
+
+    public int GetValue()
+    {
+        //Return the real value.
+        return value - offset;
+    }
+
+    public int GetRawValue()
+    {
+        //Return the secured value.
+        return value;
+    }
+
+    public void SetValue(int newValue)
+    {
+        //Generate a new offset and insert a new value.
+        Dispose();
+        offset = Random.Range(-1000, 1000);
+        value = newValue + offset;
+    }
+
+    public void Dispose()
+    {
+        //Clear our values.
+        offset = 0;
+        value = 0;
+    }
+
+    public override string ToString()
+    {
+        return GetValue().ToString();
+    }
+
+}
---
+++ Assets/myscore.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myscore : MonoBehaviour
+{
+    //Our values - One safe, one unsafe.
+    public SafeInt myScore;
+    public int nonSecure;
+
+    private void Start()
+    {
+        //Initialise our values.
+        myScore = new SafeInt(100);
+        nonSecure = 100;
+
+        //Execute our test case!
+        print("Is the non secured value changed? "+testCase(nonSecure, 100));
+        print("Is the secured value changed? " + testCase(myScore.GetRawValue(), 100));
+        print("If the program works correctly, then the secured value will be different.");
+        print("This prevents hackers from finding the value in the memory.");
+    }
+
+    void Update()
+    {
+        //Display the value.
+        GetComponent<UnityEngine.UI.Text>().text = myScore.ToString();
+    }
+
+    public void Decrease()
+    {
+        //Decrease the value by one. Called by the button.
+        myScore.SetValue((myScore.GetValue())-1);
+    }
+
+    private bool testCase(int value, int expected)
+    {
+        //Check to see if the value is changed.
+        return value != expected;
+    }
+}
